4864cdf4fa3274a7309b9decc649b7cd1f18abe3
mharif1 <md5667676@gmail.com>
2022-08-18 18:21:58 +0600

    utilities set

--- js/deposit.js
+++ js/deposit.js
@@ -1,22 +1,3 @@
-function getInputValueById(inputFirldId){
-    const inputField = document.getElementById(inputFirldId);
-    const inputFieldValueString = inputField.value;
-    const inputFieldValue = parseFloat(inputFieldValueString);
-    inputField.value = '';
-    return inputFieldValue; 
-}
-
-function getTextElementValueById(elementId){
-    const elementFiled = document.getElementById(elementId);
-    const elementFiledValueString = elementFiled.innerText;
-    const elementFiledValue = parseFloat(elementFiledValueString);
-    return elementFiledValue;
-}
-
-function setTextElementValueById(elementId, newValue){
-    const textValue = document.getElementById(elementId);
-    textValue.innerText =newValue;
-}
 
 document.getElementById('btn-deposit').addEventListener('click', function(){
     const newDepositAmount = getInputValueById('deposit-field');
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,19 @@
-console.log('utilities.js')
+function getInputValueById(inputFirldId){
+    const inputField = document.getElementById(inputFirldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue; 
+}
+
+function getTextElementValueById(elementId){
+    const elementFiled = document.getElementById(elementId);
+    const elementFiledValueString = elementFiled.innerText;
+    const elementFiledValue = parseFloat(elementFiledValueString);
+    return elementFiledValue;
+}
+
+function setTextElementValueById(elementId, newValue){
+    const textValue = document.getElementById(elementId);
+    textValue.innerText =newValue;
+}
